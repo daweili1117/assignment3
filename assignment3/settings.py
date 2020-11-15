3bcba0d034e8ae28fa5f3247bd73ff08102a9bab
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2020-11-14 20:11:01 -0600

    change setting

--- assignment3/settings.py
+++ assignment3/settings.py
@@ -153,6 +153,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
 

6f52d2d5d9ebb2864fd8d804bfc138260d122995
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2020-11-15 23:08:33 -0600

    forget password and change password

--- assignment3/settings.py
+++ assignment3/settings.py
@@ -115,13 +115,22 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'autoemailtesting9'
-EMAIL_HOST_PASSWORD = 'TESTgoahead123'
+EMAIL_HOST_USER = 'testemail01234543210'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_PASSWORD = 'goaheadTEST123'
 EMAIL_PORT = 587
+
 EMAIL_USE_TLS = True
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'testemail01234543210'
+# EMAIL_HOST_PASSWORD = 'goaheadTEST123'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
 
 # Internationalization

56be16ff58c5c50a6f4652d45ddd292a69b5d0e1
DAWEI LI <59898710+daweili1117@users.noreply.github.com>
2020-11-16 10:27:14 -0600

    last test commit

--- assignment3/settings.py
+++ assignment3/settings.py
@@ -119,18 +119,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'testemail01234543210'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_PASSWORD = 'goaheadTEST123'
+# EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'testemail01234543210'
-# EMAIL_HOST_PASSWORD = 'goaheadTEST123'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
 
 
 # Internationalization
